2278135125966de4d873bcc438efa88c1bf8fdfe
Muhammad-Musab-K <kmusab824@gmail.com>
2024-02-12 11:20:16 +0500

    initial code

--- project1/src/pages/ForgotPassword.jsx
+++ project1/src/pages/ForgotPassword.jsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { forgotPassword } from "../Firebase/Firebase"
 import { auth } from "../Firebase/Firebase"
+import { sendPasswordResetEmail } from "firebase/auth"
+
 
 
 
@@ -10,13 +12,26 @@ function ForgotPassword() {
     const [email, setEmail] = useState('');
     const [newpassword, setNewPassword] = useState('');
     const navigate = useNavigate();
-    const [confirm, setConfirm] = useState(true)
     const user = auth.currentUser;
 
     const changedPassword = async () => {
         forgotPassword(user, newpassword)
     }
-    
+    const handleResetPassword = async () => {
+        await sendPasswordResetEmail(auth, email)
+            .then(() => {
+                // Password reset email sent!
+                // Handle success, e.g., show a success message to the user
+                console.log("Password reset email sent successfully");
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // Handle error, e.g., show an error message to the user
+                console.error("Error sending password reset email: ", errorMessage);
+            });
+        console.log(email);
+    };
     return (
         <>
 
@@ -51,44 +66,7 @@ function ForgotPassword() {
                         >
                             Verify
                         </button>
-                        <div>
-
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                                    Password
-                                </label>
-
-                            </div>
-
-                            <div className="mt-2">
-                                <input
-                                    onChange={(e) => setNewPassword(e.target.value)}
-                                    disabled={confirm}
-                                    type="password"
-                                    required
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                />
-                            </div>
-
-                        </div>
-
-                        <div>
-                            <button
-                                onClick={changedPassword}
-
-                                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Reset
-                            </button>
-                            <button
-
-                                onClick={() => navigate("/signin")}
-                                className="mt-10 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            >
-                                Go back
-                            </button>
-
-                        </div>
+                       
                     </div>
 
 
